c9cc68e1ad1a0a9732cd583ec514b8a4ca3e468a
Ahmed Qamesh <ahmed.qamesh@yahoo.com>
2019-06-28 19:43:47 +0200

    Enh

---
+++ pattern_calls.h
@@ -0,0 +1,11 @@
+#ifndef pattern_calls_h
+#define pattern_calls_h
+#include<iostream> 
+using namespace std;
+class calls{ 
+tuple <int , string, string , string> 
+
+void find_call(int id, string name, string patternFile, string Flag){
+    return make_tuple(id,name, patternFile, Flag); 
+	}; 
+}
